d9a2e05274cea18a8c41356b9eff7132f2239006
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-02-05 11:54:35 -0300

    Routes to authenticate and get user's info

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import { CreateUserController } from "./controllers/CreateUserController";
+import { GetUserInfoController } from "./controllers/GetUserController";
+import { LoginUserController } from "./controllers/LoginUserController";
+import { authentication } from "./middleware/auth";
+
+const createUserController = new CreateUserController();
+const loginUserController = new LoginUserController();
+const getUserInfoController = new GetUserInfoController();
 
 const routes = Router();
+
+routes.post('/users/create', createUserController.handle);
+
+routes.post("/login", loginUserController.handle);
+
+routes.get("/users/profile", authentication, getUserInfoController.handle);
 
 export { routes };
