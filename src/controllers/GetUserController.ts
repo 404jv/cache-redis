aed53881257cd42a7a494707d6f327883fb8c381
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-02-05 21:12:07 -0300

    Find user in Redis before connect to database

--- src/controllers/GetUserController.ts
+++ src/controllers/GetUserController.ts
@@ -1,18 +1,25 @@
 import { Request, Response } from "express";
 import { createConnection } from "../postgres";
+import { getRedis } from "../redisConfig";
 
 export class GetUserInfoController {
   async handle(request: Request, response: Response) {
     const { userId } = request;
 
-    const clientConnection = await createConnection();
+    const userJsonRedis = await getRedis(`user-${userId}`);
 
-    const { rows } = await clientConnection.query(
-      `SELECT * FROM USERS WHERE ID  = $1 LIMIT 1`,
-      [userId]
-    );
+    let user = JSON.parse(userJsonRedis);
 
-    const user = rows[0];
+    if (!user) {
+      const clientConnection = await createConnection();
+
+      const { rows } = await clientConnection.query(
+        `SELECT * FROM USERS WHERE ID  = $1 LIMIT 1`,
+        [userId]
+      );
+
+      user = rows[0];
+    }
 
     return response.json(user);
   }
