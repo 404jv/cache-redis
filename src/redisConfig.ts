b5a9375c09b9858cfc413891a62eb67062f53125
João Victor R. Alves <joaovictorramalho7@gmail.com>
2022-02-05 21:10:51 -0300

    Init redis

---
+++ src/redisConfig.ts
@@ -0,0 +1,17 @@
+import Redis from 'ioredis';
+import { promisify } from 'util';
+
+const redisClient = new Redis();
+
+function getRedis(value: string) {
+  const syncRedisGet = promisify(redisClient.get).bind(redisClient);
+  return syncRedisGet(value);
+}
+
+function setRedis(key: string, value: string) {
+  const syncRedisSet = promisify(redisClient.set).bind(redisClient);
+  return syncRedisSet(key, value);
+}
+
+
+export { redisClient, getRedis, setRedis };
